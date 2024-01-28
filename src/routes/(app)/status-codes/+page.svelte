ff53cabc46157d7f2d79c0a8446850139a496915
Mark Nardi <nardimark7@gmail.com>
2024-01-28 20:08:33 +0100

    update status code list for mobile

--- src/routes/(app)/status-codes/+page.svelte
+++ src/routes/(app)/status-codes/+page.svelte
@@ -3,16 +3,20 @@
     import StatusCodeCard from '$lib/components/StatusCodeCard.svelte';
     import { Input } from '$lib/components/ui/input/index.js';
     import { ToggleGroup, ToggleGroupItem } from '$lib/components/ui/toggle-group';
+    import Toggle from '$lib/components/ui/toggle/toggle.svelte';
     import { metaTags } from '$lib/constants/metadata.constant.js';
     import { STATUS_CODES } from '$lib/constants/status-codes.constant.js';
     import type { StatusCode } from '$lib/server/status/status-code.type.js';
     import { activeUser } from '$lib/stores/user.store.js';
+    import { isMobile } from '$lib/stores/window.store.js';
+    import { Filter, ListFilter } from 'lucide-svelte';
     import { MetaTags } from 'svelte-meta-tags';
 
     export let data;
 
     let selectedStatusCodes = ['4xx', '5xx'];
     let searchTerm = '';
+    let showFilter = false;
 
     let groupedStatusCodes: Record<string, StatusCode[]> = {};
     $: groupedStatusCodes = data.statusCodes.reduce((acc, statusCode) => {
@@ -43,43 +47,60 @@
 
         return statusCode.code.toLowerCase().includes(searchTerm.toLowerCase()) || STATUS_CODES[statusCode.code]?.title.toLowerCase().includes(searchTerm.toLowerCase());
     }
+
+    $: console.log(showFilter);
+    
 </script>
 
 
 <MetaTags {...metaTags} title='Status codes' />
 
 <status-codes-view>
-    <div class="w-full flex flex-row justify-between align-center max-w-[1000px]">
-        <h2 class="lg:text-3xl font-bold text-xl">Status codes</h2>
-        
-        <div class="flex gap-4 items-center">
-            <Input placeholder="Search status codes" bind:value={searchTerm} />
-            <p class="text-sm">Filter:</p>
-            <ToggleGroup type="multiple" bind:value={selectedStatusCodes}>
-                <ToggleGroupItem value="1xx" aria-label="1xx">
-                    1xx
-                </ToggleGroupItem>
-                <ToggleGroupItem value="2xx" aria-label="2xx">
-                    2xx
-                </ToggleGroupItem>
-                <ToggleGroupItem value="3xx" aria-label="3xx">
-                    3xx
-                </ToggleGroupItem>
-                <ToggleGroupItem value="4xx" aria-label="4xx">
-                    4xx
-                </ToggleGroupItem>
-                <ToggleGroupItem value="5xx" aria-label="5xx">
-                    5xx
-                </ToggleGroupItem>
-              </ToggleGroup>
+    <header-section>
+        <div class="flex gap-8 items-center">
+            <h2 class="lg:text-3xl font-bold text-xl flex-shrink-0">Status codes</h2>
+            {#if $isMobile}
+                <div class="flex gap-2 items-center">
+                    <Toggle bind:pressed={showFilter} >
+                        <ListFilter class="w-4 h-4" />
+                    </Toggle>
+                    <Input placeholder="Search..." bind:value={searchTerm} />
+                </div>
+            {/if}
         </div>
-    </div>
+        
+        {#if !$isMobile || showFilter}
+            <div class="flex gap-4 items-center">
+                {#if !$isMobile}
+                    <Input placeholder="Search status codes" bind:value={searchTerm} />
+                {/if}
+                <p class="text-sm">Filter:</p>
+                <ToggleGroup type="multiple" bind:value={selectedStatusCodes}>
+                    <ToggleGroupItem value="1xx" aria-label="1xx">
+                        1xx
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="2xx" aria-label="2xx">
+                        2xx
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="3xx" aria-label="3xx">
+                        3xx
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="4xx" aria-label="4xx">
+                        4xx
+                    </ToggleGroupItem>
+                    <ToggleGroupItem value="5xx" aria-label="5xx">
+                        5xx
+                    </ToggleGroupItem>
+                </ToggleGroup>
+            </div>
+        {/if}
+    </header-section>
 
     <div class="w-full h-full flex flex-row justify-center items-start overflow-auto">
         <list-container>
             {#each Object.keys(groupedStatusCodes) as statusCodesGroupKey}
                 <div class="flex flex-col items-start gap-4">
-                    <h3 class="text-2xl">{statusCodesGroupKey}</h3>
+                    <h3 class="lg:text-2xl text-lg">{statusCodesGroupKey}</h3>
                     <status-code-list>
                         {#each groupedStatusCodes[statusCodesGroupKey] as statusCode}
                             {@const code = $activeUser?.receivedCodes.find(code => code.code === statusCode.code)}
@@ -97,18 +118,22 @@
 
 <style lang="postcss">
     status-codes-view {
-        @apply w-full h-full py-6;
-        @apply flex flex-col justify-center items-center gap-8;
+        @apply w-full h-full lg:py-6 py-4;
+        @apply flex flex-col justify-center items-center lg:gap-8 gap-4;
+    }
+
+    header-section {
+        @apply w-full max-w-[1000px] lg:px-6 px-4;
+        @apply flex lg:flex-row flex-col gap-4 justify-between items-start;
     }
 
     list-container {
-        @apply w-full flex flex-col gap-8  max-w-[1000px];
+        @apply w-full flex flex-col gap-8 max-w-[1000px] lg:px-6 px-4;
     }
 
     status-code-list {
-        @apply w-full ;
-        @apply grid gap-8;
-        grid-template-columns: repeat(auto-fill, minmax(8rem, 1fr));
-        grid-auto-rows: 1fr;
+        @apply w-full;
+        @apply grid lg:grid-cols-6 grid-cols-3;
+        @apply lg:gap-8 gap-4;
     }
 </style>
