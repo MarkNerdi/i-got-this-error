ed38e6f1b096a4a8a2e4cb455193729e2e324162
Mark Nardi <nardimark7@gmail.com>
2024-02-02 15:23:14 +0100

    add metatitle to main route

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,9 +3,13 @@
     import RarestCodes from '$lib/components/dashboard/RarestCodes.svelte';
     import StatusOfTheDay from '$lib/components/dashboard/StatusOfTheDay.svelte';
     import WhatIsThis from '$lib/components/dashboard/WhatIsThis.svelte';
+    import { metaTags } from '$lib/constants/metadata.constant.js';
+    import { MetaTags } from 'svelte-meta-tags';
     
     export let data;
 </script>
+
+<MetaTags {...metaTags} title='Track your progress' />
 
 <home-view>
     <div class="w-full grid gap-16 grid-cols-6">
